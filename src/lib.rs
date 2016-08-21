2003ee245e9f8ec24e155403d1cf48c4ff9903fd
Alexander Ronald Altman <alexanderaltman@me.com>
2016-08-21 12:07:29 -0700

    The core `Invariant` type

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,48 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::*;
+
+#[derive(Debug,Clone,Copy)]
+pub struct Invariant<T: ?Sized, F = Box<FnMut(<T as ToOwned>::Owned) -> bool>>
+    where T: ToOwned, F: FnMut(<T as ToOwned>::Owned) -> bool {
+    check: F,
+    inner: T,
+}
+
+impl<T, F> Invariant<T, F>
+    where T: ToOwned, F: FnMut(<T as ToOwned>::Owned) -> bool {
+    pub fn into_inner(self) -> T { self.inner }
+
+    pub fn try_from_inner(inner: T, mut check: F) -> Option<Self> {
+        if check(inner.to_owned()) {
+            Some(Invariant {
+                check: check,
+                inner: inner,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ?Sized, F> Invariant<T, F>
+    where T: ToOwned, F: FnMut(<T as ToOwned>::Owned) -> bool {
+    pub fn as_inner_ref(&self) -> &T { &self.inner }
+
+    pub fn as_inner_mut(&mut self) -> &mut T { &mut self.inner }
+}
+
+impl<T: fmt::Display + ?Sized, F> fmt::Display for Invariant<T, F>
+    where T: ToOwned, F: FnMut(<T as ToOwned>::Owned) -> bool {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        <T as fmt::Display>::fmt(self.as_inner_ref(), formatter)
+    }
+}
+
+impl<T: hash::Hash + ?Sized, F> hash::Hash for Invariant<T, F>
+    where T: ToOwned, F: FnMut(<T as ToOwned>::Owned) -> bool {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        <T as hash::Hash>::hash(self.as_inner_ref(), state)
     }
 }
